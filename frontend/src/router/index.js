282794be97df23e1a4e97404b13c9ac57674fae7
Ángel Luis Litago Falces <alitago@MacBook-Pro-de-Angel.local>
2026-03-23 21:43:01 +0000

    Modify iddent

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,20 +1,31 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { useAuth } from "../composables/useAuth";
 
-import LoginView    from "../views/LoginView.vue";
-import EquipoView   from "../views/EquipoView.vue";
+import LoginView from "../views/LoginView.vue";
+import EquipoView from "../views/EquipoView.vue";
 import DashboardView from "../views/DashboardView.vue";
-import PerfilView   from "../views/PerfilView.vue";
+import PerfilView from "../views/PerfilView.vue";
 import ResetPasswordView from "../views/ResetPasswordView.vue";
 
-
 const routes = [
-  { path: "/",          redirect: "/equipo" },
-  { path: "/login",     component: LoginView,     meta: { publica: true } },
-  { path: "/equipo",    component: EquipoView,    meta: { requiereAuth: true, soloAdmin: true } },
-  { path: "/dashboard", component: DashboardView, meta: { requiereAuth: true, soloAdmin: true } },
-  { path: "/perfil",    component: PerfilView,    meta: { requiereAuth: true } },
-  { path: "/reset-password", component: ResetPasswordView, meta: { publica: true } },
+  { path: "/", redirect: "/equipo" },
+  { path: "/login", component: LoginView, meta: { publica: true } },
+  {
+    path: "/equipo",
+    component: EquipoView,
+    meta: { requiereAuth: true, soloAdmin: true },
+  },
+  {
+    path: "/dashboard",
+    component: DashboardView,
+    meta: { requiereAuth: true, soloAdmin: true },
+  },
+  { path: "/perfil", component: PerfilView, meta: { requiereAuth: true } },
+  {
+    path: "/reset-password",
+    component: ResetPasswordView,
+    meta: { publica: true },
+  },
 ];
 
 const router = createRouter({
